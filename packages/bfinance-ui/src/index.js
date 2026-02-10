fdc883b070d434fee78a75f44a2389734db98320
Yana <sorokotyagina.yana@gmail.com>
2026-02-11 00:48:18 +0300

    set up ui component library and storybook docs

---
+++ packages/bfinance-ui/src/index.js
@@ -0,0 +1,6 @@
+export * from "./components/Button";
+export * from "./components/Dropdown";
+export * from "./components/Input";
+export * from "./components/Status";
+
+import "./styles/tokens.css";
