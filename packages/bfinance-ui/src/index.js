2013fd75789163a9b02d570ff80ad7f8b65278fb
Yana <sorokotyagina.yana@gmail.com>
2026-02-24 16:14:15 +0300

    Added Textarea

--- packages/bfinance-ui/src/index.js
+++ packages/bfinance-ui/src/index.js
@@ -6,5 +6,6 @@ export * from "./components/Checkbox";
 export * from "./components/Modal";
 export * from "./components/Tooltip";
 export * from "./components/Toggle";
+export * from "./components/Textarea";
 
 import "./styles/tokens.css";
